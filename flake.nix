23af8b1001c68e2521521a8553c31fb0c4536458
Linus Gasser <linus.gasser@epfl.ch>
2025-11-12 11:23:21 +0100

    Adding latest versions

--- flake.nix
+++ flake.nix
@@ -12,6 +12,8 @@
     in {
       packages = {
         noir = pkgs.callPackage ./noir.nix {};
+        barretenberg_v2_1_2 = pkgs.callPackage ./barretenberg_v2.1.2.nix {};
+        noir_v1_0_0_beta_15 = pkgs.callPackage ./noir_v1.0.0_beta.15.nix {};
         barretenberg = pkgs.callPackage ./barretenberg.nix {};
         default = pkgs.callPackage ./noir.nix {};
       };
